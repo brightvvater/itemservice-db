560b339f3c8c9cc2827a7f5adf6d790888aa0297
hyunsoo cho <hscho199463@gmail.com>
2023-01-23 16:55:31 +0900

    springDataJpa

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 
 
 @Slf4j
-@Import(JpaConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
